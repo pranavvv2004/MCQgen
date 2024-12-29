16d6c55ba460d06e0593c413ac258d946f1f6eb5
pranav mete <pranavmete57@gamil.com>
2024-12-30 00:25:29 +0530

    structure updated

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
- 
+from setuptools import find_packages, setup
+
+setup(
+
+name='mcqgenrator',
+version='0.0.1',
+author='pranav mete',
+author_email='pranav mete.com',
+install_requires=["openai","langchain", "streamlit", "python-dotenv", "PyPDF2"],
+packages=find_packages()
+)
